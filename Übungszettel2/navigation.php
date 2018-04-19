19b3d7d6c29cbf4bf123bc9d196ec20645b66e91
kirill <Kirill.Antropov@gmx.net>
2018-04-19 13:18:46 +0200

    startseite text

---
+++ Übungszettel2/navigation.php
@@ -0,0 +1,10 @@
+<?php
+    echo "
+    <ul>
+    <li><a href="startseite.html">Startseite</a></li>
+    <li><a href="#about">Über uns</a></li>
+    <li><a href="#archiv">Archiv</a></li>
+    <li><a href="#profil">Profil</a></li>
+    <li style="float:right"> <a href="registration.html">Registrieren</a></li>
+</ul>";
+>
