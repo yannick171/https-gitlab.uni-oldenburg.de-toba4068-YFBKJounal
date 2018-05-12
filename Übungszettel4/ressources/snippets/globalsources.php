dc20815e6ebb8c65fdc3545dc5c8242cdf1602f8
kirill <Kirill.Antropov@gmx.net>
2018-05-12 16:29:01 +0200

    Bootstrap spielereien mit dem Login popup

--- Übungszettel4/ressources/snippets/globalsources.php
+++ Übungszettel4/ressources/snippets/globalsources.php
@@ -1,9 +1,13 @@
 <link rel="stylesheet" type="text/css" href="ressources/css/defaultstyle.css">
-<link rel="stylesheet" type="text/css" href="ressources/css/footer.css">
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300" rel="stylesheet">
 <meta charset="utf-8">
 
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Google Icons -->
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 
 <!-- Bootstrap -->
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="ressources/css/bootstrap.css">
--- Übungszettel4/ressources/snippets/head.php
+++ Übungszettel4/ressources/snippets/head.php
@@ -1,8 +1,9 @@
+
   <div class="login">
       <ul>
         <div class="defaultstyle">
         <li>
-          <a href="*.html"> Anmelden</a>
+          <a data-toggle="modal" data-target="#exampleModalCenter"> Anmelden</a>
         </li>
         <li>
           <a href="registration.php"> Registrieren</a>
@@ -14,9 +15,40 @@
       <img id="titelbild" src="ressources/images/banner.jpg" alt="Titelbild">
     </header>
 
-    <div class="nav_main">
+    <!-- Beispiel Modal aus https://getbootstrap.com/docs/4.0/components/modal/#tooltips-and-popovers -->
+    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLongTitle">Anmeldung</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <form>
+              <div class="form-group">
+            <label for="email" class="col-form-label">E-Mail:</label>
+            <input type="email" class="form-control" id="usr">
+          </div>
+          <div class="form-group">
+            <label for="password" class="col-form-label">Passwort:</label>
+            <input type="password" class="form-control" id="pwd"></textarea>
+          </div>
+        </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Schließen</button>
+            <button type="button" class="btn btn-primary">Einloggen</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+<nav class="nav_main">
+<div class="defaultstyle">
       <ul>
-        <div class="defaultstyle">
         <li>
           <a href="startseite.php">
                     Startseite
@@ -37,6 +69,12 @@
                       Profil
                   </a>
         </li>
-        </div>
+        <li style="float:right;">
+          <form>
+            <button style="" class="btn btn-primary" type="button"> <i class="material-icons">search</i> </button>
+          </form>
+        </li>
       </ul>
-    </div>
+</div>
+
+</nav>
