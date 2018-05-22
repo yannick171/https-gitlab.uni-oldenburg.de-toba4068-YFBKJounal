a01c30894189bc233d00f7ca8492215c7abd6ba0
Fenno Boomgaarden <fenno.boomgaarden@t-online.de>
2018-05-22 13:56:42 +0200

    Added countdown to redakteur.php

--- Übungszettel4/redakteur.php
+++ Übungszettel4/redakteur.php
@@ -14,8 +14,39 @@
 
 	<body>
 
-		  <?php include ("ressources/snippets/head.php") ;?>
+		 <?php include ("ressources/snippets/head.php") ;?>
 
+		<script>
+			// Set the date we're counting down to
+			var countDownDate = new Date("Sep 5, 2018 15:37:25").getTime();
+	
+			// Update the count down every 1 second
+			var x = setInterval(function() {
+	
+				// Get todays date and time
+				var now = new Date().getTime();
+				
+				// Find the distance between now an the count down date
+				var distance = countDownDate - now;
+				
+				// Time calculations for days, hours, minutes and seconds
+				var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+				var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+				var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+				var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+				
+				// Output the result in an element with id="demo"
+				document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+				+ minutes + "m " + seconds + "s ";
+				
+				// If the count down is over, write some text 
+				if (distance < 0) {
+					clearInterval(x);
+					document.getElementById("demo").innerHTML = "EXPIRED";
+				}
+			}, 1000);
+		</script>
+		  
 		<main class="defaultstyle">
 			<h1>Redakteur Profil</h1>
             <article>
@@ -191,8 +222,9 @@
                 </div>
             </div>
             <h1>
-                Nächste Ausgabe
+                Nächste Ausgabe: <p id="countdown"></p>
             </h1>
+			
 			<div class = "nextMagazine">
                 <div class = "parentContainer">
                 
