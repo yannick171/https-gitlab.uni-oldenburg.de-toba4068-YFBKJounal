2de6e11b8c92ed053372b8ad246e979fddab1299
Yannick Sunou <yannick.watat.sunou@uni-oldenburg.de>
2018-05-22 13:41:54 +0200

    neue Änderung4

--- Übungszettel4/autor.php
+++ Übungszettel4/autor.php
@@ -2,153 +2,161 @@
 <html>
     <head>
         <?php include("ressources/snippets/globalsources.php") ?>
-
         <link rel="stylesheet" href="ressources/autorseite/Mystyle.css"/>
-         <link rel="stylesheet" href="ressources/css/article.css"/>
+        <link rel="stylesheet" media="screen and (max-width: 768px)" href="ressources/autorseite/kleine_resolution.css" />
 
    </head>
   <body>
 
          <?php include ("ressources/snippets/head.php") ;?>
 
-            <main class="defaultstyle">
-                <article>
-                    <ul>
-                       <h1> Autor Profil</h1>
-                        </ul>
-                    <section class="grid-container-article">
-                    <div>
-                        <img class="profilpic" src="ressources/redakteurseite/profilbild.png" alt="picture"/>
+       <main class="defaultstyle">
+         <div id="Haupttitel1">
+            <h1> Meine Beiträge</h1>
+         </div>
+		 <section id="Mysection">
+			    <aside>
+			         <div class="asideitem1">
+ 
+					 </div>
+					 <div class="Profilbild">
+						<p> Yannick Watat Sunou</p>
+						<img src="ressources/autorseite/yannick.png" alt="Photo de Profild" width="250" height="250" />
+						 <p>yannick.watat.sunou@uni-oldenburg.de.</p>
+                    <a href="https://de-de.facebook.com/login/"><img src="ressources/autorseite/facebook.png" alt="Facebook" /></a>
+						 <a href="https://twitter.com/login?lang=de"><img src="ressources/autorseite/twitter.png" alt="Twitter" /></a>
+					 </div>
+			    </aside>
+			    <article>
+                       <div class="asideitem1">
+                          <h1> angenommene Artikeln</h1>
+					   </div>
+			      <div id="section1"> 
+					<div id="banniere_image" style="background-image: url(ressources/archivseite/TitelSeite4.jpg);">
+					   <div id="banniere_description">
+						   <p> Der Weg des Computers zur Kreativität...  </p>
+						   <a href="ressources/archivseite/Zeitung%202.txt" class="bouton_rouge">Siehe l'article <img src="ressources/autorseite/flecheblanchedroite.png" alt="" /></a>
+					   </div>
+					</div>
+					<div id="banniere_image" style="background-image: url(ressources/archivseite/TitelSeite2.jpg);">
+					   <div id="banniere_description">
+						   <p> Der Weg des Computers zur Kreativität...  </p>
+						   <a href="ressources/archivseite/Zeitung%202.txt" class="bouton_rouge">Siehe l'article <img src="ressources/autorseite/flecheblanchedroite.png" alt="" /></a>
+					   </div>
+					</div>
+					<div id="banniere_image" style="background-image: url(ressources/archivseite/TitelSeite3.jpg);">
+					   <div id="banniere_description">
+						   <p> Die Grenzen der Computational Intelligence...  </p>
+						   <a href="ressources/archivseite/Zeitung%202.txt" class="bouton_rouge">Siehe l'article <img src="ressources/autorseite/flecheblanchedroite.png" alt="" /></a>
+					   </div>
+					</div>
+					<div id="banniere_image" style="background-image: url(ressources/archivseite/TitelSeite5.jpg);">
+					   <div id="banniere_description">
+						   <p> Der Weg des Computers zur Kreativität...  </p>
+						   <a href="ressources/archivseite/Zeitung%202.txt" class="bouton_rouge">Siehe l'article <img src="ressources/autorseite/flecheblanchedroite.png" alt="" /></a>
+					   </div>
+					</div>
+					<div id="banniere_image" style="background-image: url(ressources/archivseite/TitelSeite6.jpg);">
+					   <div id="banniere_description">
+						   <p> Der Weg des Computers zur Kreativität...  </p>
+						   <a href="ressources/archivseite/Zeitung%202.txt" class="bouton_rouge">Siehe l'article <img src="ressources/autorseite/flecheblanchedroite.png" alt="" /></a>
+					   </div>
+					</div>
+					<div id="banniere_image" style="background-image: url(ressources/archivseite/TitelSeite.jpg);">
+					   <div id="banniere_description">
+						   <p> Der Weg des Computers zur Kreativität...  </p>
+						   <a href="ressources/archivseite/Zeitung%202.txt" class="bouton_rouge">Siehe l'article <img src="ressources/autorseite/flecheblanchedroite.png" alt="" /></a>
+					   </div>
+					</div>
+				</div>
 
-                    </div>
-                     <div>
-                          <p class ="item2">
-                                        Name:<br>
-                                          Yannick Watat <br><br>
-                                        Email:<br>
-                                           yannick.watat.sunou@uni-oldenburg.de <br><br>
+					<div class="asideitem1">
+                          <h1> zu bearbeitungen Artikeln</h1>
+					 </div>
+						 <div id="section1"> 
+					<div id="banniere_image" style="background-image: url(ressources/archivseite/TitelSeite4.jpg);">
+					   <div id="banniere_description">
+						   <p> Der Weg des Computers zur Kreativität...  </p>
+						   <a href="ressources/archivseite/Zeitung%202.txt" class="bouton_rouge">Siehe l'article <img src="ressources/autorseite/flecheblanchedroite.png" alt="" /></a>
+					   </div>
+					</div>
+					<div id="banniere_image" style="background-image: url(ressources/archivseite/TitelSeite2.jpg);">
+					   <div id="banniere_description">
+						   <p> Der Weg des Computers zur Kreativität...  </p>
+						   <a href="ressources/archivseite/Zeitung%202.txt" class="bouton_rouge">Siehe l'article <img src="ressources/autorseite/flecheblanchedroite.png" alt="" /></a>
+					   </div>
+					</div>
+					<div id="banniere_image" style="background-image: url(ressources/archivseite/TitelSeite3.jpg);">
+					   <div id="banniere_description">
+						   <p> Die Grenzen der Computational Intelligence...  </p>
+						   <a href="ressources/archivseite/Zeitung%202.txt" class="bouton_rouge">Siehe l'article <img src="ressources/autorseite/flecheblanchedroite.png" alt="" /></a>
+					   </div>
+					</div>
+					<div id="banniere_image" style="background-image: url(ressources/archivseite/TitelSeite5.jpg);">
+					   <div id="banniere_description">
+						   <p> Der Weg des Computers zur Kreativität...  </p>
+						   <a href="ressources/archivseite/Zeitung%202.txt" class="bouton_rouge">Siehe l'article <img src="ressources/autorseite/flecheblanchedroite.png" alt="" /></a>
+					   </div>
+					</div>
+					<div id="banniere_image" style="background-image: url(ressources/archivseite/TitelSeite6.jpg);">
+					   <div id="banniere_description">
+						   <p> Der Weg des Computers zur Kreativität...  </p>
+						   <a href="ressources/archivseite/Zeitung%202.txt" class="bouton_rouge">Siehe l'article <img src="ressources/autorseite/flecheblanchedroite.png" alt="" /></a>
+					   </div>
+					</div>
+					<div id="banniere_image" style="background-image: url(ressources/archivseite/TitelSeite.jpg);">
+					   <div id="banniere_description">
+						   <p> Der Weg des Computers zur Kreativität...  </p>
+						   <a href="ressources/archivseite/Zeitung%202.txt" class="bouton_rouge">Siehe l'article <img src="ressources/autorseite/flecheblanchedroite.png" alt="" /></a>
+					   </div>
+					</div>
+				</div>
 
-                                        Titel:<br>
-                                            noch nicht<br><br>
-                          </p>
-                    </div>
-             <div class="item-down-article">
-                       <ul>
-                          <a>veröffentliche Beiträge </a>
+					<div class="asideitem1">
+                          <h1> abgelehnte Artikeln</h1>
+					 </div>
+					 <div id="section1"> 
+					<div id="banniere_image" style="background-image: url(ressources/archivseite/TitelSeite4.jpg);">
+					   <div id="banniere_description">
+						   <p> Der Weg des Computers zur Kreativität...  </p>
+						   <a href="ressources/archivseite/Zeitung%202.txt" class="bouton_rouge">Siehe l'article <img src="ressources/autorseite/flecheblanchedroite.png" alt="" /></a>
+					   </div>
+					</div>
+					<div id="banniere_image" style="background-image: url(ressources/archivseite/TitelSeite2.jpg);">
+					   <div id="banniere_description">
+						   <p> Der Weg des Computers zur Kreativität...  </p>
+						   <a href="ressources/archivseite/Zeitung%202.txt" class="bouton_rouge">Siehe l'article <img src="ressources/autorseite/flecheblanchedroite.png" alt="" /></a>
+					   </div>
+					</div>
+					<div id="banniere_image" style="background-image: url(ressources/archivseite/TitelSeite3.jpg);">
+					   <div id="banniere_description">
+						   <p> Die Grenzen der Computational Intelligence...  </p>
+						   <a href="ressources/archivseite/Zeitung%202.txt" class="bouton_rouge">Siehe l'article <img src="ressources/autorseite/flecheblanchedroite.png" alt="" /></a>
+					   </div>
+					</div>
+					<div id="banniere_image" style="background-image: url(ressources/archivseite/TitelSeite5.jpg);">
+					   <div id="banniere_description">
+						   <p> Der Weg des Computers zur Kreativität...  </p>
+						   <a href="ressources/archivseite/Zeitung%202.txt" class="bouton_rouge">Siehe l'article <img src="ressources/autorseite/flecheblanchedroite.png" alt="" /></a>
+					   </div>
+					</div>
+					<div id="banniere_image" style="background-image: url(ressources/archivseite/TitelSeite6.jpg);">
+					   <div id="banniere_description">
+						   <p> Der Weg des Computers zur Kreativität...  </p>
+						   <a href="ressources/archivseite/Zeitung%202.txt" class="bouton_rouge">Siehe l'article <img src="ressources/autorseite/flecheblanchedroite.png" alt="" /></a>
+					   </div>
+					</div>
+					<div id="banniere_image" style="background-image: url(ressources/archivseite/TitelSeite.jpg);">
+					   <div id="banniere_description">
+						   <p> Der Weg des Computers zur Kreativität...  </p>
+						   <a href="ressources/archivseite/Zeitung%202.txt" class="bouton_rouge">Siehe l'article <img src="ressources/autorseite/flecheblanchedroite.png" alt="" /></a>
+					   </div>
+					</div>
+				</div>
 
-                        </ul>
-                    <ul>
-                  <table id="listartikel">
-					     <tr>
-						<th>Datum </th>
-                        <th>Titel </th>
-                        <th>Autor/inen</th>
-                        <th>Status</th>
-					</tr>
-					  <tr>
-						<td>
-							<a>2018</a>
-                         </td>
-                        <td>
-                            <a href ="ressources/archivseite/Zeitung1/Zeitung%201.txt">
-                            Die Grenzen der Computational Intelligence
-							</a>
-                        </td>
-                        <td>
-                            <a> teo Nagelmann </a>
-                        </td>
-                         <td>
-                            <a id= "ressources/archivseite/Zeitung%202.txt">
-								angenommen
-							</a>
-                        </td>
-
-					</tr>
-                      <tr>
-						<td>
-							<a>2018</a>
-                         </td>
-                        <td>
-                            <a href = "ressources/archivseite/Zeitung1/temporale-Logik.txt">
-								Der Weg des Computers zur Kreativität
-							</a>
-                        </td>
-                        <td><a>hauptvogel Ann</a></td>
-                        <td><a>angenommen</a></td>
-
-					</tr>
-                        <tr>
-						   <td>
-							<a>	2017</a>
-                          </td>
-                        <td>
-                            <a href = "ressources/archivseite/Zeitung%202.txt">
-								Gläserne Menschen
-							</a>
-                        </td>
-                        <td>
-                            <a>
-								peter Hen
-							</a>
-                        </td>
-                        <td>
-                            <a>
-								in Bearbeitung
-							</a>
-                        </td>
-
-					    </tr>
-                    </table>
-                        </ul>
-                 <ul>
-                    <summary><b>Neuer Beitrag einreichen</b></summary><br>
-                <label> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Titel:
-                   <input name="name" value=":" type="text" size="31" maxlength="33"><br>
-                </label>
-                        <br>
-                 <label> Autor/in:
-                   <input name="name" value="" type="text" size="31" maxlength="33"><br><br>
-                 </label>
-                    <br>
-
-                     <label>Datei hinzufügen:
-                         <form action="/actions_page.php" >
-                          <input list ="liste" type="file" name="img" multiple><br>
-                          <input list ="liste" type="file" name="img" multiple><br>
-                          <input list ="liste" type="file" name="img" multiple><br>
-                         </form>
-                     </label>
-                        <footer class="lign">&nbsp; </footer>
-                        <p><strong>Checklist für Beiträge</strong> <br><br>
-                            <input type="checkbox" name="opt[]"
-                            value="parken"> Der Beitrag ist bisher unveröffentlicht und wurde auch keiner anderen Zeitschrift vorgelegt.<br><br>
-                            <input type="checkbox" name="opt[]"
-                            value="parken"> Der Text folgt den stilistischen und bibliografischen Vorgaben, die in den jeweiligen <a href="Rubrikenrichtlinien" > Rubrikenrichtlinien</a> zu fin­den sind.<br><br>
-                           <input type="checkbox" name="opt[]"
-                            value="parken"> Der oder die Autor_in versichert, die allgemein gültigen Standards wissenschaftlicher Arbeit berücksichtigt und sämtliche genutzte Bilder, Grafiken und Texte Dritter kenntlich gemacht zu haben machen.
-                        </p>
-                      <footer class="lign">&nbsp; </footer>
-                        <p><strong>Copyright-Vermerk </strong><br><br>
-                           Autor_innen, die in dieser Zeitschrift publizieren möchten, stimmen den folgenden Bedingungen zu:<br>
-                            <input type="checkbox" name="opt[]"
-                            value="parken"> Die Autor/innen stimmen den Bestimmungen dieser Copyright-Regelungen zu, die für diesen Beitrag im Falle einer Veröffentlichung Anwendung finden. (Kommentare für die Redaktion können weiter unten angefügt werden.)<br><br>
-                        </p>
-
-                        <p>
-                        Erklärung zum Schutz persönlicher Daten in dieser Zeitschrift
-                            <br><br><br>
-                        Namen und E-Mail-Adressen, die in diesem Onlineangebot eingegeben werden (bspw. bei der Registrierung von Nutzern), werden ausschließlich zu den angegebenen Zwecken genutzt und nicht an Dritte weitergegeben.
-                        Namen der Autor_innen werden mit den Artikeln veröffentlicht.
-                        </p>
-                        <footer class="lign">&nbsp; </footer>
-                          <form action="/actions_page.php" >
-                             <input type="submit">
-                           </form>
-                     </ul>
-                 </div>
-                       </section>
-                </article>
-             </main><br><br>
+			    </article>
+		  </section>
+     </main><br> 
 
     <?php include ("ressources/snippets/footer.php") ;?>
     <?php include ("ressources/snippets/loadjavascript.php") ;?>
