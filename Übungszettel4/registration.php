e8c3dfbb8c0e58392297d6f71fef883764ba8042
Fenno Boomgaarden <fenno.boomgaarden@t-online.de>
2018-05-25 15:27:40 +0200

    Added formvalid.js registration.php validates form now before submit

--- Übungszettel4/registration.php
+++ Übungszettel4/registration.php
@@ -6,37 +6,37 @@
         <link rel = "stylesheet" type="text/css" href = "ressources/registrierungsseite/registrierung_style_sheet.css">
     </head>
     <body>
+		<script src = "ressources/js/formvalid.js"></script>
         <?php include ("ressources/snippets/head.php");?>
         <main class="defaultstyle">
             <div class ="container">
                 <h2>Registration</h2>
-                <form action="/action_page.php">
+                <form action="/action_page.php" id="registerform">
                     <div class="form-group">
                         <label for="Name">Name:</label>
-                        <input type="text" class="form-control" placeholder="Name">
+                        <input type="text" class="form-control" id="fname" placeholder="Vorname" name="firstname">
                     </div>
                     <div class="form-group">
                         <label for="Vorname">Vorname:</label>
-                        <input type="text" class="form-control" placeholder="Vorname">
+                        <input type="text" class="form-control" id="lname" placeholder="Nachname" name="lastname">
                     </div>
                     <div class="form-group">
                         <label for="email">Email:</label>
                         <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
                     </div>
                     <div class="form-group">
-                        <label for="pwd">Password:</label>
-                        <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+                        <label for="pwd2">Passwort:</label>
+                        <input type="password" class="form-control" id="pwd2" placeholder="Enter password" name="pswd">
                     </div>
                     <div class="form-group">
-                        <label for="pwd">Password wiederholen:</label>
-                        <input type="password" class="form-control" id="pwd" placeholder="Retype password" name="pswd">
+                        <label for="pwdcnf">Passwort wiederholen:</label>
+                        <input type="password" class="form-control" id="pwdcnf" placeholder="Retype password" name="pswdcnf">
                     </div>
-                    <p>
+                    <p id = "error">
                     </p>
-                    <button type="submit" class="btn btn-success">
-                        Registrieren
-                    </button>
+                    
                 </form>
+				<button onclick="validateForm()">Registrieren</button>
             </div>
         </main>
         <?php include ("ressources/snippets/footer.php") ;?>
---
+++ Übungszettel4/ressources/js/formvalid.js
@@ -0,0 +1,33 @@
+//Validiert die Registrierung
+function validateForm()
+{
+	//Alles eingegeben?
+	if(	document.getElementById("fname").value.length == 0 || 
+		document.getElementById("lname").value.length == 0 ||
+		document.getElementById("email").value.length == 0 ||
+		document.getElementById("pwd2").value.length == 0 ||
+		document.getElementById("pwdcnf").value.length == 0)
+	{
+		document.getElementById("error").innerHTML = "Bitte alle Felder ausfüllen!";
+		document.getElementById("error").style="color:red;";
+	}
+	else
+	{
+		if(document.getElementById("pwd2").value.length < 6)
+		{
+			document.getElementById("error").innerHTML = "Das Passwort muss mindestens 6 Zeichen lang sein!";
+			document.getElementById("error").style="color:red;";
+		}
+		else if(document.getElementById("pwd2").value != document.getElementById("pwdcnf").value)
+		{
+			document.getElementById("error").innerHTML = "Die Passwörter stimmen nicht überein!";
+			document.getElementById("error").style="color:red;";
+		}
+		else
+		{
+			document.getElementById("registerform").submit()
+			
+			window.open("autor.php","_self")
+		}
+	}
+}
