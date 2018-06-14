ff2bf292c9e9caa0e13bd68bd337523777c14b11
Unknown <Kirill.Antropov@gmx.net>
2018-06-14 22:53:39 +0200

    Profil ist änderbar

---
+++ Übungszettel6/ressources/snippets/changeProfile.php
@@ -0,0 +1,28 @@
+<?php
+    session_start();
+
+    if(isset($_POST)){
+        $db = new PDO("sqlite:../SQLData/user.db");
+        $id = $_SESSION["userId"];
+
+        print_r($_POST);
+        isset($_POST["email"]) ? $_SESSION["email"] = $_POST["email"] : "";
+        isset($_POST["vorname"]) ? $_SESSION["vorname"] = $_POST["vorname"]: "";
+        isset($_POST["nachname"]) ? $_SESSION["nachname"] = $_POST["nachname"]: "";
+        isset($_POST["infoText"]) ?  $_SESSION["infoText"] = $_POST["infoText"]: "" ;
+
+        print_r($_SESSION);
+        $stmt = $db ->prepare("UPDATE user SET email=:newEmail, lastName =:newLastname, firstName =:newFirstname, infoText =:newInfotext WHERE id='$id'");
+
+        $stmt ->bindValue(":newEmail",$_SESSION["email"],PDO::PARAM_STR);
+        $stmt ->bindValue(":newLastname", $_SESSION["nachname"],PDO::PARAM_STR);
+        $stmt ->bindValue(":newFirstname", $_SESSION["vorname"],PDO::PARAM_STR);
+        $stmt ->bindValue(":newInfotext",$_SESSION["infoText"],PDO::PARAM_STR);
+
+
+        $stmt -> execute();
+        $stmt = NULL;
+
+        header("Location: ../../autor.php");
+        exit();
+    }
--- Übungszettel6/ressources/snippets/profile.php
+++ Übungszettel6/ressources/snippets/profile.php
@@ -26,23 +26,23 @@
       <!--- Profil ende -->
       <script src = "ressources/js/changeProfile.js"></script>
       <!-- Profil bearbeiten -->
-      <form method="post" action="asd">
+      <form onsubmit="" method="post" action="ressources/snippets/changeProfile.php">
       <div class="wrapper" id="autorProfilBearbeiten">
         <div class="changeProfileButton">
           <a href="#" onclick="$(this).closest('form').submit()" class="zustimmen"> Bestätigen </a>
-          <a class="verwerfen"> Verwerfen </a>
+          <a href="autor.php" class="verwerfen"> Verwerfen </a>
         </div>
         <div class="metadata">
 
         </div>
         <div class="autorInfobox autorInformationStyle">
-          <strong>Name: </strong> <input class="form-control" type="text" name="email" value="<?php echo $_SESSION["vorname"]; ?>"><br><br>
+          <strong>Name: </strong> <input class="form-control" type="text" name="nachname" value="<?php echo $_SESSION["vorname"]; ?>"><br><br>
           <strong>Vorname: </strong><input class="form-control" type="text" name="vorname" value="<?php echo $_SESSION["nachname"]; ?>"><br><br>
-          <strong>E-Mail: </strong><input class="form-control" type="email" name="nachname" value="<?php echo $_SESSION["email"]; ?>"><br><br>
+          <strong>E-Mail: </strong><input class="form-control" type="email" name="email" value="<?php echo $_SESSION["email"]; ?>"><br><br>
         </div>
         <div class="<div class="autorIntrobox autorInformationStyle">
           <p><strong>Was wollt ihr den Besuchern Ihres Profils mitteilen?:</strong></p>
-          <textarea style="font-size: 20px;" class="form-control" rows="9" name="abstract"><?php echo $_SESSION["infoText"]; ?></textarea>
+          <textarea style="font-size: 20px;" class="form-control" rows="9" name="infoText"><?php echo $_SESSION["infoText"]; ?></textarea>
         </div>
     </div>
     </form>    
