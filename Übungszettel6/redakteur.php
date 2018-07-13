1d093fb4b3333c89b5dd791d84f4967f785a8684
Benjamin Stahr <BenjaminStahr97@web.de>
2018-07-13 12:43:58 +0200

    Hier ist das Ablehnen von Artikeln eingebaut

--- Übungszettel6/redakteur.php
+++ Übungszettel6/redakteur.php
@@ -95,7 +95,6 @@ if (isset($_POST['bildUrl'])) {
 <?php include("ressources/snippets/head.php"); ?>
 
 
-
 <main class="defaultstyle">
     <h1>Redakteur Profil</h1>
     <article>
@@ -144,217 +143,215 @@ if (isset($_POST['bildUrl'])) {
     </h1>
 
     <h1>Arbeitsbereich</h1>
+    <div class="parentContainer">
 
+        <div class="containerElement">
+            <h2 id="zuPrüfen" class=workingArea>
+                zu prüfen
+            </h2>
 
-    <form id="toProofList">
+            <ul id="todoArticles" class="list-group">
+
+                <?php
+                foreach ($toProof as $proof) {
+                    $id = uniqid();
+                    echo '<li class = "list-group-item">'
+                        . '<div class="card margin">'
+                        . '<div class="card-header">'
+                        . '<h5 class="mb-0">'
+                        . '<button class="btn btn-link collapsed" data-toggle="collapse" data-target="#' . $id . '" aria-expanded="false" aria-controls="' . $id . '">'
+                        . $proof["title"]
+                        . '</h5>'
+                        . '</div>'
+                        . '<div id="' . $id . '" class="collapse" aria-labelledby="headingFour">'
+                        . '<div class="card-body">'
+                        . $proof["abstract"]
+                        . '</div>'
+                        . '<button onclick="addArticleToProofed(' . $proof["id"] . ')" class="btn btn-success btn-block">Artikel als geprüft markieren</button>'
+                        . '<button onclick="addArticleToDeclinedToProof(' .$proof["id"] . ')" class="btn btn-danger btn-block">Artikel ablehnen</button>'
+                        . '</div>'
+                        . '</div>'
+                        . '<input type="hidden" name="id" value="' . $proof["id"] . '">'
+                        . '</li>';
+                }
+                ?>
+            </ul>
+        </div>
+
+        <input type="hidden" name="seperator" value="a">
+        <div class="containerElement">
+            <h2 id="geprüft" class="workingArea">
+                geprüft
+            </h2>
+            <p id = "test"></p>
+            <ul id="acceptedArticles" class="list-group">
+                <?php
+                foreach ($proofed as $proof) {
+                    $id = uniqid();
+                    echo '<li class = "list-group-item">'
+                        . '<div class="card margin">'
+                        . '<div class="card-header">'
+                        . '<h5 class="mb-0">'
+                        . '<button class="btn btn-link collapsed" data-toggle="collapse" data-target="#' . $id . '" aria-expanded="false" aria-controls="' . $id . '">'
+                        . $proof["title"]
+                        . '</h5>'
+                        . '</div>'
+                        . '<div id="' . $id . '" class="collapse" aria-labelledby="headingFour">'
+                        . '<div class="card-body">'
+                        . $proof["abstract"]
+                        . '</div>'
+                        . '<button onclick="addArticleToMagazine(' . $proof["id"] . ')" class="btn btn-success btn-block">Artikel zur nächsten Ausgabe hinzufügen</button>'
+                        . '<button onclick="addArticleToDeclinedProofed(' . $proof["id"] . ')" class="btn btn-danger btn-block">Artikel ablehnen</button>'
+                        . '</div>'
+                        . '</div>'
+                        . '<input type="hidden" name="id" value="' . $proof["id"] . '">'
+                        . '</li>';
+                }
+                ?>
+            </ul>
+        </div>
+    </div>
+
+    <h1>
+        Nächste Ausgabe:
+    </h1>
+
+    <div class="nextMagazine">
         <div class="parentContainer">
-
             <div class="containerElement">
-                <h2 id="zuPrüfen" class=workingArea>
-                    zu prüfen
-                </h2>
-                <ul id="todoArticles" class="list-group">
-
-                    <?php
-                    foreach ($toProof as $proof) {
-                        $id = uniqid();
-                        echo '<li class = "list-group-item">'
-                            . '<div class="card margin">'
-                            . '<div class="card-header">'
-                            . '<h5 class="mb-0">'
-                            . '<button class="btn btn-link collapsed" data-toggle="collapse" data-target="#' . $id . '" aria-expanded="false" aria-controls="' . $id . '">'
-                            . $proof["title"]
-                            . '</h5>'
-                            . '</div>'
-                            . '<div id="' . $id . '" class="collapse" aria-labelledby="headingFour">'
-                            . '<div class="card-body">'
-                            . $proof["abstract"]
-                            . '</div>'
-                            . '<button onclick="addArticleToProofed('.$proof["id"].')" class="btn btn-success btn-block">Artikel als geprüft markieren</button>'
-                            . '</div>'
-                            . '</div>'
-                            . '<input type="hidden" name="id" value="' . $proof["id"] . '">'
-                            . '</li>';
-                    }
-                    ?>
-                </ul>
-            </div>
-
-            <input type = "hidden" name = "seperator" value="a">
-            <div class="containerElement">
-                <h2 id="geprüft" class="workingArea">
-                    geprüft
-                </h2>
-                <p id="testDerId"></p>
-                <ul id="acceptedArticles" class="list-group">
-                    <?php
-                    foreach ($proofed as $proof) {
-                        $id = uniqid();
-                        echo '<li class = "list-group-item">'
-                            . '<div class="card margin">'
-                            . '<div class="card-header">'
-                            . '<h5 class="mb-0">'
-                            . '<button class="btn btn-link collapsed" data-toggle="collapse" data-target="#' . $id . '" aria-expanded="false" aria-controls="' . $id . '">'
-                            . $proof["title"]
-                            . '</h5>'
-                            . '</div>'
-                            . '<div id="' . $id . '" class="collapse" aria-labelledby="headingFour">'
-                            . '<div class="card-body">'
-                            . $proof["abstract"]
-                            . '</div>'
-                            . '<button onclick="addArticleToMagazine('.$proof["id"].')" class="btn btn-success btn-block">Artikel zur nächsten Ausgabe hinzufügen</button>'
-                            . '</div>'
-                            . '</div>'
-                            . '<input type="hidden" name="id" value="' . $proof["id"] . '">'
-                            . '</li>';
-                    }
-                    ?>
-                </ul>
-            </div>
-        </div>
-
-    </form>
-
-
-        <h1>
-            Nächste Ausgabe:
-        </h1>
-
-        <div class="nextMagazine">
-            <div class="parentContainer">
-                <div class="containerElement">
-                    <div class="containerChild">
-                        <h3>
-                            Titel der Ausgabe
-                        </h3>
-                        <p id="nextMagazineTitle">
-                            <?php echo $newestMagazine["title"]; ?>
-                        </p>
-                        <div class="centerButton">
-                            <button type="button" data-toggle="modal" data-target="#modalTitle">
-                                Titel ändern
-                            </button>
-                        </div>
-                    </div>
-                    <div class="containerChild">
-                        <h3>
-                            Titelbild der Ausgabe
-                        </h3>
-                        <img src="ressources/archivseite/TitelSeite.jpg" alt="Titelseite des Magazins"
-                             class="centerImage">
-                        <div class="centerButton">
-                            <button type="button" data-toggle="modal" data-target="#modalImage">Bild ändern</button>
-                        </div>
-                    </div>
-                    <div class="containerChild">
-                        <h3>
-                            Einleitungstext der Ausgabe
-                        </h3>
-                        <p>
-                            <?php echo $newestMagazine["description"]; ?>
-                        </p>
-                        <div class="centerButton">
-                            <button type="button" data-toggle="modal" data-target="#modalDescription">
-                                Beschreibung ändern
-                            </button>
-                        </div>
-
-                    </div>
-                </div>
-                <div class="containerElement">
+                <div class="containerChild">
                     <h3>
-                        Inhalt
+                        Titel der Ausgabe
                     </h3>
-                    <ul id="articleList" class="list-group">
-                        <?php
-                        foreach ($nextMagazine as $next) {
-                            $id = uniqid();
-                            echo '<li class = "list-group-item">'
-                                . '<div class="card margin">'
-                                . '<div class="card-header">'
-                                . '<h5 class="mb-0">'
-                                . '<button class="btn btn-link collapsed" data-toggle="collapse" data-target="#' . $id . '" aria-expanded="false" aria-controls="' . $id . '">'
-                                . $next["title"]
-                                . '</h5>'
-                                . '</div>'
-                                . '<div id="' . $id . '" class="collapse" aria-labelledby="headingFour">'
-                                . '<div class="card-body">'
-                                . $next["abstract"]
-                                . '</div>'
-                                . '<button onclick="addArticleToProofedFromNextMagazine('.$next["id"].')" class="btn btn-danger btn-block">Artikel nach geprüft zurücklegen</button>'
-                                . '</div>'
-                                . '</div>'
-                                . '<input type="hidden" name="id" value="' . $next["id"] . '">'
-                                . '</li>';
+                    <p id="nextMagazineTitle">
+                        <?php echo $newestMagazine["title"]; ?>
+                    </p>
+                    <div class="centerButton">
+                        <button type="button" data-toggle="modal" data-target="#modalTitle">
+                            Titel ändern
+                        </button>
+                    </div>
+                </div>
+                <!--div class="containerChild">
+                    <h3>
+                        Titelbild der Ausgabe
+                    </h3>
+                    <img src="ressources/archivseite/TitelSeite.jpg" alt="Titelseite des Magazins"
+                         class="centerImage">
+                    <div class="centerButton">
+                        <button type="button" data-toggle="modal" data-target="#modalImage">Bild ändern</button>
+                    </div>
+                </div-->
+                <div class="containerChild">
+                    <h3>
+                        Einleitungstext der Ausgabe
+                    </h3>
+                    <span style="display: block; word-wrap:break-word;">
+                        <?php echo $newestMagazine["description"]; ?>
+                    </span>
+                    <div class="centerButton">
+                        <button type="button" data-toggle="modal" data-target="#modalDescription">
+                            Beschreibung ändern
+                        </button>
+                    </div>
 
-                        }
-                        ?>
-                    </ul>
                 </div>
             </div>
-        </div>
-        <button type="submit" onclick="publish()" class="btn btn-primary btn-block">Ausgabe veröffentlichen</button>
-        <!-- Here are the Modals-->
+            <div class="containerElement">
+                <h3>
+                    Inhalt
+                </h3>
+                <ul id="articleList" class="list-group">
+                    <?php
+                    foreach ($nextMagazine as $next) {
+                        $id = uniqid();
+                        echo '<li class = "list-group-item">'
+                            . '<div class="card margin">'
+                            . '<div class="card-header">'
+                            . '<h5 class="mb-0">'
+                            . '<button class="btn btn-link collapsed" data-toggle="collapse" data-target="#' . $id . '" aria-expanded="false" aria-controls="' . $id . '">'
+                            . $next["title"]
+                            . '</h5>'
+                            . '</div>'
+                            . '<div id="' . $id . '" class="collapse" aria-labelledby="headingFour">'
+                            . '<div class="card-body">'
+                            . $next["abstract"]
+                            . '</div>'
+                            . '<button onclick="addArticleToProofedFromNextMagazine(' . $next["id"] . ')" class="btn btn-warning btn-block">Artikel nach geprüft zurücklegen</button>'
+                            . '<button onclick="addArticleToDeclinedNextMagazine(' . $next["id"] . ')" class="btn btn-danger btn-block">Artikel ablehnen</button>'
+                            . '</div>'
+                            . '</div>'
+                            . '<input type="hidden" name="id" value="' . $next["id"] . '">'
+                            . '</li>';
 
-        <!-- Modal -->
-        <div class="modal fade" id="modalTitle" role="dialog">
-            <div class="modal-dialog">
-                <!-- Modal content-->
-                <div class="modal-content">
-                    <form method="post" action= <?php $_SERVER['PHP_SELF'] ?>>
-                        <div class="modal-body">
-                            <p>Geben Sie den neuen Titel ein : </p>
-                            <input name="newTitle" class="form-control mr-sm-2" type="text" placeholder="..."
-                                   aria-label="text">
-                        </div>
-                        <div class="modal-footer">
-                            <button type="submit" class="btn btn-default">Anwenden</button>
-                        </div>
-                    </form>
-                </div>
+                    }
+                    ?>
+                </ul>
             </div>
         </div>
+    </div>
+    <button type="submit" onclick="publish()" class="btn btn-primary btn-block">Ausgabe veröffentlichen</button>
+    <!-- Here are the Modals-->
 
-        <!-- Modal -->
-        <div class="modal fade" id="modalImage" role="dialog">
-            <div class="modal-dialog">
-                <!-- Modal content-->
-                <div class="modal-content">
-                    <form method="post" action= <?php $_SERVER['PHP_SELF'] ?>>
-                        <div class="modal-body">
-                            <p>Geben Sie die neue URL des Bildes an : </p>
-                            <input name="bildUrl" class="form-control mr-sm-2" type="text" placeholder="..."
-                                   aria-label="text">
-                        </div>
-                        <div class="modal-footer">
-                            <button type="submit" class="btn btn-default">Anwenden</button>
-                        </div>
-                    </form>
-                </div>
+    <!-- Modal -->
+    <div class="modal fade" id="modalTitle" role="dialog">
+        <div class="modal-dialog">
+            <!-- Modal content-->
+            <div class="modal-content">
+                <form method="post" action= <?php $_SERVER['PHP_SELF'] ?>>
+                    <div class="modal-body">
+                        <p>Geben Sie den neuen Titel ein : </p>
+                        <input name="newTitle" class="form-control mr-sm-2" type="text" placeholder="..."
+                               aria-label="text">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-default">Anwenden</button>
+                    </div>
+                </form>
             </div>
         </div>
-        <!--PHP for action on change path of picture-->
+    </div>
 
-        <!-- Modal -->
-        <div class="modal fade" id="modalDescription" role="dialog">
-            <div class="modal-dialog">
-                <!-- Modal content-->
-                <div class="modal-content">
-                    <form method="post" action= <?php $_SERVER['PHP_SELF'] ?>>
-                        <div class="modal-body">
-                            <p>Geben Sie eine neue Beschreibung an : </p>
-                            <div class="form-group">
-                                <label for="discription">neue Beschreibung : </label>
-                                <textarea class="form-control" rows="5" id="comment" name="newDescription"></textarea>
-                            </div>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="submit" class="btn btn-default">Anwenden</button>
-                        </div>
-                    </form>
-                </div>
+    <!-- Modal>
+    <div class="modal fade" id="modalImage" role="dialog">
+        <div class="modal-dialog">
+            <!-- Modal content>
+            <div class="modal-content">
+                <form method="post" action= <!--?php $_SERVER['PHP_SELF'] ?>>
+                    <div class="modal-body">
+                        <p>Geben Sie die neue URL des Bildes an : </p>
+                        <input name="bildUrl" class="form-control mr-sm-2" type="text" placeholder="..."
+                               aria-label="text">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-default">Anwenden</button>
+                    </div>
+                </form>
             </div>
         </div>
+    </div>
+    <!--PHP for action on change path of picture-->
+
+    <!-- Modal for changeing the description-->
+    <div class="modal fade" id="modalDescription" role="dialog">
+        <div class="modal-dialog">
+            <!-- Modal content-->
+            <div class="modal-content">
+                <form method="post" action= <?php $_SERVER['PHP_SELF'] ?>>
+                    <div class="modal-body">
+                        <p>Geben Sie eine neue Beschreibung an : </p>
+                        <div class="form-group">
+                            <label for="discription">neue Beschreibung : </label>
+                            <textarea class="form-control" rows="5" id="comment" name="newDescription"></textarea>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-default">Anwenden</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 
 
 </main>
