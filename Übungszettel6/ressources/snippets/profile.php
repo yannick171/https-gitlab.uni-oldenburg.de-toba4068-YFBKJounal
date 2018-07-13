22d677d63687cc88a6180e98453e4a31378ab51d
Yannick Sunou <yannick.watat.sunou@uni-oldenburg.de>
2018-07-13 09:25:52 +0200

    profile.php Änderung

--- Übungszettel6/ressources/snippets/profile.php
+++ Übungszettel6/ressources/snippets/profile.php
@@ -88,9 +88,9 @@
                  <div>
         <p>
             <input type="button" id="button1" value="G" onclick="insertTag('<strong>','</strong>','infotextInput');" />
-            <input type="button" id="button2" value="I" onclick="insertTag('<span>','</span>','infotextInput');" />
+            <input type="button" id="button2" value="H1" onclick="insertTag('<h1>','</h1>','infotextInput');" />
             <input type="button" id="button3" value="Links" onclick="insertTag('<a>','</a>','infotextInput');" />
-            <input type="button" id="button4" value="h1" onclick="insertTag('<h1>','</h1>','infotextInput');"/>
+            <input type="button" id="button4" value="H2" onclick="insertTag('<h2>','</h2>','infotextInput');"/>
 			<input type="button" id="button5" value="P" onclick="insertTag('<p>','</p>','infotextInput');"/>
 			<input type="button" id="button6" value="Ul" onclick="insertTag('<ul>','</ul>','infotextInput');"/>
 			<input type="button" id="button7" value="Ol" onclick="insertTag('<ol>','</ol>','infotextInput');"/>
